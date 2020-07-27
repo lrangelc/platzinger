2f09ea49fcb96e24939444115fee9315e3c27c3b
Luis Rangel Castro <luisrangelc@rpaipos.com>
2020-07-27 12:57:24 -0600

    fix home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from '../interfaces/user';
+import { AuthenticationService } from '../services/authentication.service';
 import { UserService } from './../services/user.service';
 
 @Component({
@@ -8,10 +9,33 @@ import { UserService } from './../services/user.service';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  user: User;
   friends: User[] = [];
   query: string = '';
 
-  constructor(private userService: UserService) {
+  constructor(
+    private userService: UserService,
+    private authenticationService: AuthenticationService
+  ) {
+    this.authenticationService.getStatus().subscribe(
+      (status) => {
+        this.userService
+          .getUserById(status.uid)
+          .valueChanges()
+          .subscribe(
+            (data: User) => {
+              this.user = data;
+            },
+            (err) => {
+              console.error(err);
+            }
+          );
+      },
+      (err) => {
+        console.error(err);
+      }
+    );
+
     userService
       .getUsers()
       .valueChanges()
--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,15 +1,48 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../interfaces/user';
+import { AuthenticationService } from '../services/authentication.service';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.scss']
+  styleUrls: ['./profile.component.scss'],
 })
 export class ProfileComponent implements OnInit {
+  user: User;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private userService: UserService,
+    private authenticationService: AuthenticationService
+  ) {
+    this.authenticationService.getStatus().subscribe(
+      (status) => {
+        this.userService
+          .getUserById(status.uid)
+          .valueChanges()
+          .subscribe(
+            (data: User) => {
+              this.user = data;
+            },
+            (err) => {
+              console.error(err);
+            }
+          );
+      },
+      (err) => {
+        console.error(err);
+      }
+    );
   }
 
+  ngOnInit(): void {}
+  saveSettings(): void {
+    this.userService
+      .editUser(this.user)
+      .then(() => {})
+      .catch((err) => {
+        console.error(err);
+        alert('Hubo un error al guardar los cambios!');
+      });
+  }
 }
